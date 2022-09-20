2e58a96d410a8431b55d4b7d0f0ad384ed341c5f
unknown <rahulmk.94802@gmail.com>
2022-09-20 19:20:35 +0530

    Mathematical Opearator

---
+++ Mathematical-Operators-Assignment-2/problem2.js
@@ -0,0 +1,13 @@
+// - You have to take five numbers, stored in the variables with the following names
+//     one, two, three, four, five
+// - All the values are doubled, that is, they are multiplied by 2
+
+let one=1*2;
+let two=2*2;
+let three=3*2;
+let four=4*2;
+let five=5*2;
+
+let sum=one+two+three+four+five;
+
+console.log(sum);
---
+++ Mathematical-Operators-Assignment-2/problem3.js
@@ -0,0 +1,22 @@
+// - You have to take five numbers, stored in variables with the following names
+    
+//      one, two, three, four, five, six
+
+// The value stored in the variable`two, five, six`has been doubled, such that if initially the value was 3, it has become 6
+
+// The value stored in the variable`three, four`has been increased three times, such that if initially it was 3, it has become 9
+
+// The value stored in the variable `one` has not been changed.
+
+// Find the sum of the updated values stored in`one, two, three, four, five, six`
+
+let one=1;
+let two=2*2;
+let three=3*3;
+let four=4*3;
+let five=5*2;
+let six=6*2;
+
+let sum=one+two+three+four+five+six;
+
+console.log(sum);
